58fa1b0b2e027f1107b0370283e10d109ccb9d80
RYNEEER <ricardogabriel759@gmail.com>
2019-10-24 20:31:20 -0500

    ttt

--- app/src/main/java/ariel/az/devcode20/profileActivity.java
+++ app/src/main/java/ariel/az/devcode20/profileActivity.java
@@ -1,8 +1,12 @@
 package ariel.az.devcode20;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class profileActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class profileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.editarperfil, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
